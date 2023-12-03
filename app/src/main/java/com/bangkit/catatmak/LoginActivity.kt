c25d1604d189f32b9dfabee8d97e27fad950b39b
Kamiko0714 <dewaathallah2003@gmail.com>
2023-12-03 20:31:29 +0700

    baru xml yg dikerjain ~kamiko

---
+++ app/src/main/java/com/bangkit/catatmak/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.bangkit.catatmak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
